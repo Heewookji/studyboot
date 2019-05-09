56062606091071a321b50a997ec5164464df6662
jihwan8D <jihwan8D@gmail.com>
2019-05-09 18:27:24 +0900

    space 매퍼, .js 생성

--- src/main/java/com/studyboot/sms/domain/Space.java
+++ src/main/java/com/studyboot/sms/domain/Space.java
@@ -1,5 +1,7 @@
 package com.studyboot.sms.domain;
 
+import java.util.List;
+
 public class Space {
   
   private int no;
@@ -9,42 +11,62 @@ public class Space {
   private String tel;
   private String intro;
   
-  //private List<SpacePhoto> files;
-  
+  private List<SpacePhoto> files;
+
   public int getNo() {
     return no;
   }
+
   public void setNo(int no) {
     this.no = no;
   }
+
   public String getName() {
     return name;
   }
+
   public void setName(String name) {
     this.name = name;
   }
+
   public int getAddress() {
     return address;
   }
+
   public void setAddress(int address) {
     this.address = address;
   }
+
   public String getAddressDetail() {
     return addressDetail;
   }
+
   public void setAddressDetail(String addressDetail) {
     this.addressDetail = addressDetail;
   }
+
   public String getTel() {
     return tel;
   }
+
   public void setTel(String tel) {
     this.tel = tel;
   }
+
   public String getIntro() {
     return intro;
   }
+
   public void setIntro(String intro) {
     this.intro = intro;
   }
+
+  public List<SpacePhoto> getFiles() {
+    return files;
+  }
+
+  public void setFiles(List<SpacePhoto> files) {
+    this.files = files;
+  }
+  
 }
