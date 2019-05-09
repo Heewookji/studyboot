182e9f625fd980a33282847f5eef7aae21fa0c56
hyunwoo <hwim1202@gmail.com>
2019-05-09 17:18:34 +0900

    spaceDao,spaceController,spaceService..

---
+++ src/main/java/com/studyboot/sms/dao/SpaceDao.java
@@ -0,0 +1,24 @@
+package com.studyboot.sms.dao;
+
+import java.util.List;
+import java.util.Map;
+import com.studyboot.sms.domain.Space;
+
+public interface SpaceDao {
+  
+  int insert(Space space);
+  List<Space> findAll(Map<String,Object> params);
+  Space findByNo(int no);
+  int increaseCount(int no);
+  int update(Space space);
+  int delete(int no);
+  int countAll();
+  
+}
+
+
+
+
+
+
+
