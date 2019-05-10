3fcdf7adc5e53a240ec563a5a0c98d8acdb5a233
jihwan8D <jihwan8D@gmail.com>
2019-05-10 13:46:32 +0900

    .

---
+++ src/main/java/com/studyboot/sms/domain/SpaceTag.java
@@ -0,0 +1,27 @@
+package com.studyboot.sms.domain;
+
+public class SpaceTag {
+  
+  private int no;
+  private int spaceNo;
+  private String name;
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public int getSpaceNo() {
+    return spaceNo;
+  }
+  public void setSpaceNo(int spaceNo) {
+    this.spaceNo = spaceNo;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
