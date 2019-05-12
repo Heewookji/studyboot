d58c615395033379ac3466ba566c849555fa9e44
82105 <sangmin.P42@gmail.com>
2019-05-12 19:38:13 +0900

    스터디 리스트 수정

--- src/main/webapp/html/study/index.js
+++ src/main/webapp/html/study/index.js
@@ -1,33 +1,56 @@
-var tbody = $('tbody'),
-  templateSrc = $('#tr-template').html(), // script 태그에서 템플릿 데이터를 꺼낸다.
-  trGenerator = Handlebars.compile(templateSrc); 
+var pageNo = 1,
+pageSize = 3,
+tbody = $('tbody'),
+templateSrc = $('#tr-template').html(), // script 태그에서 템플릿 데이터를 꺼낸다.
+trGenerator = Handlebars.compile(templateSrc); 
 
-// JSON 형식의 데이터 목록 가져오기
-function loadList() {
+//JSON 형식의 데이터 목록 가져오기
+function loadList(pn) {
 
-  $.getJSON('../../app/json/study/list', function(obj) {
-
+  $.getJSON('../../app/json/study/list?pageNo=' + pn + '&pageSize=' + pageSize,
+      function(obj) {
     
-    // 템플릿 엔진을 실행하여 tr 태그 목록을 생성한다. 그리고 바로 tbody에 붙인다.
+    if (pageNo == obj.totalPage) {
+      return;
+    }
+    // 서버에 받은 데이터 중에서 페이지 번호를 글로벌 변수에 저장한다.
+    pageNo = obj.pageNo;
+
     $(trGenerator(obj)).appendTo(tbody);
-    
- // 데이터 로딩이 완료되면 body 태그에 이벤트를 전송한다.
+
+    // 데이터 로딩이 완료되면 body 태그에 이벤트를 전송한다.
     $(document.body).trigger('loaded-list');
-    
+
   });
 };
 
 
-// 페이지를 출력한 후 1페이지를 로딩한다.
-loadList();
+//페이지를 출력한 후 1페이지를 로딩한다.
+loadList(1);
+
+$(window).scroll(function(obj) {
+  if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+
+    loadList(pageNo + 1);
+
+  }
+});
+
 
 $(document.body).bind('loaded-list', () => {
   $('.study-view-link').click((e) => {
     e.preventDefault();
     window.location.href = 'view.html?no=' +
-      $(e.target).attr('data-no');
+    $(e.target).attr('data-no');
   });
 });
+
+
+
+
+
+
+
 
 
 
