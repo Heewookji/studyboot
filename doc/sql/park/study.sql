42592fc0b36cb22150a2aae91b15487d75b080e0
ppappikko <sanghyun.dev@gmail.com>
2019-05-13 12:11:06 +0900

    db 수정

--- doc/sql/park/study.sql
+++ doc/sql/park/study.sql
@@ -65,5 +65,25 @@ insert into sms_std_cls_s(cls_s_id, cls_m_id, name) values(14, 11, 'servlet');
 insert into sms_std_cls_s(cls_s_id, cls_m_id, name) values(15, 11, 'list');
 
 
+-- 회원 평점 정보
+-- 회원 평점 정보 키, 회원 번호 , 평점 , 평가일
+
+insert into sms_member_rate_info(member_id, rate) values(2, 4.0);
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(2, 3.0, 20190513);
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(2, 2.5, 20190513);
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(2, 4.0, 20191013);
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(2, 4.5, 20191013);
+
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(3, 3.5, 20190520);
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(3, 5.0, 20190520);
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(3, 2.0, 20190520);
+
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(4, 1.0, 20190713);
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(4, 2.0, 20190714);
+
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(5, 4.0, 20190514);
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(5, 4.0, 20190515);
+insert into sms_member_rate_info(member_id, rate, rate_dt) values(5, 4.0, 20190516);
+
 
 
