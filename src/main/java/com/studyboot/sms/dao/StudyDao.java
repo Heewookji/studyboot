293da8140a2d3a853ee0f4537d1e2f02c5fd1315
ppappikko <sanghyun.dev@gmail.com>
2019-05-14 16:08:43 +0900

    스터디 구성원 총 평점 업데이트 기능 추가

--- src/main/java/com/studyboot/sms/dao/StudyDao.java
+++ src/main/java/com/studyboot/sms/dao/StudyDao.java
@@ -9,7 +9,9 @@ public interface StudyDao {
   int insert(Study study);
   List<Study> findAll(Map<String,Object> param);
   Study findByNo(int no);
+  double[] findMemberRateByNo(int no);
   //int increaseCount(int no);
+  int updateRate(Study study);
   int update(Study study);
   int delete(int no);
   int countAll();
