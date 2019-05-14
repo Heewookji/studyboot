59f03c6b8e827cb67729b909c8a8c6e5180ea082
hyunwoo <hwim1202@gmail.com>
2019-05-14 18:13:21 +0900

    .

--- src/main/webapp/html/space/view.js
+++ src/main/webapp/html/space/view.js
@@ -1,7 +1,5 @@
 var param = location.href.split('?')[1],
 section = $('section'),
-templateSrc = $('#div-template').html(),
-trGenerator = Handlebars.compile(templateSrc),
 i = 0;
 
 if (param) {
@@ -22,11 +20,28 @@ function loadDetail(no) {
       alert("에러가 발생했습니다.");
     }
   });
-
+  
   $.getJSON('../../app/json/space/detail?no=' + no, 
       function(obj) {
-    $(trGenerator(obj)).appendTo(section);
-
+  	var templateTag = $('#div-tag').html();
+  	var trTagGenerator = Handlebars.compile(templateTag);
+    $(trTagGenerator(obj)).appendTo($(tag));
+    
+  	var templateConvName = $('#div-convName').html();
+  	var trConvNameGenerator = Handlebars.compile(templateConvName);
+    $(trConvNameGenerator(obj)).appendTo($(convName));
+    
+   	var templateSpacePhoto = $('#div-spacePhoto').html();
+  	var trSpacePhotoGenerator = Handlebars.compile(templateSpacePhoto);
+    $(trSpacePhotoGenerator(obj)).appendTo($(filePath));
+    
+   	var templateRoomInfo = $('#div-roomInfo').html();
+  	var trRoomInfoGenerator = Handlebars.compile(templateRoomInfo);
+    $(trRoomInfoGenerator(obj)).appendTo($(roomInfo));
+    
+   	var templateSpaceReview = $('#div-spaceReview').html();
+  	var trSpaceReviewGenerator = Handlebars.compile(templateSpaceReview);
+    $(trSpaceReviewGenerator(obj)).appendTo($(spaceReview));
   });
 
 }
