82bcbb4291e7332ec46f405b742e68698c9b54d1
hyunwoo <hwim1202@gmail.com>
2019-05-16 18:04:27 +0900

    .

--- src/main/java/com/studyboot/sms/web/json/SpaceReviewController.java
+++ src/main/java/com/studyboot/sms/web/json/SpaceReviewController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.studyboot.sms.domain.SpaceReview;
 import com.studyboot.sms.service.SpaceService;
