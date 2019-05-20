819f5e951d56fa59ea0a82b6c5880e18b5449a37
heewookji <wlgmldnr@daum.net>
2019-05-20 18:29:28 +0900

    .

---
+++ SpaceTag.java
@@ -0,0 +1,33 @@
+package com.studyboot.sms.domain;
+
+public class SpaceTag {
+  
+  private int no;
+  private int spaceNo;
+  private String tagName;
+  
+  @Override
+  public String toString() {
+    return "SpaceTag [no=" + no + ", spaceNo=" + spaceNo + ", tagName=" + tagName + "]";
+  }
+  
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public int getSpaceNo() {
+    return spaceNo;
+  }
+  public void setSpaceNo(int spaceNo) {
+    this.spaceNo = spaceNo;
+  }
+  public String getTagName() {
+    return tagName;
+  }
+  public void setTagName(String tagName) {
+    this.tagName = tagName;
+  }
+  
+}
