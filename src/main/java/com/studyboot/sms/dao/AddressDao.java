d02b0dad7809ebbb7ed845b270da2b4e5477d0aa
heewookji <wlgmldnr@daum.net>
2019-05-20 18:20:07 +0900

    address filter -back

--- src/main/java/com/studyboot/sms/dao/AddressDao.java
+++ src/main/java/com/studyboot/sms/dao/AddressDao.java
@@ -1,14 +1,12 @@
 package com.studyboot.sms.dao;
 
+import java.util.HashMap;
 import java.util.List;
 import com.studyboot.sms.domain.Address;
 
 public interface AddressDao {
-  List<Address> findLargeAddressName();
-  List<Address> findMediumAddressName(String addressNo);
-  List<Address> findSmallAddressName(String addressNo);
   Address findFullAddressName(int no);
-  List<Address> findAddressName();
+  List<Address> findAddressName(HashMap<String, Object> params);
 }
 
 
--- src/main/java/com/studyboot/sms/service/impl/AddressServiceImpl.java
+++ src/main/java/com/studyboot/sms/service/impl/AddressServiceImpl.java
@@ -1,5 +1,6 @@
 package com.studyboot.sms.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import com.studyboot.sms.dao.AddressDao;
@@ -18,7 +19,25 @@ public class AddressServiceImpl implements AddressService {
 
   @Override
   public List<Address> addressList(String addressNo) {
-    return addressDao.findAddressName();
+    
+    HashMap<String, Object> params = new HashMap<>();
+    
+    params.put("addressNo", addressNo);
+    params.put("addressSize", addressNo.length());
+    
+    List<Address> list = addressDao.findAddressName(params);
+    
+    for(Address a : list) {
+      if(a.getAddressLargeNo() == null)
+        a.setAddressLargeNo("");
+      if(a.getAddressMediumNo() == null)
+        a.setAddressMediumNo("");
+      if(a.getAddressSmallNo() == null)
+        a.setAddressSmallNo("");
+      a.setAddressNo(a.getAddressLargeNo()+a.getAddressMediumNo()+a.getAddressSmallNo());
+    }
+    
+    return list;
   }
   
   
