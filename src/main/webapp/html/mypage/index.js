a5e4bf8c72532ff15aad05758a8dfbc9ef557f61
ppappikko <sanghyun.dev@gmail.com>
2019-05-25 18:27:55 +0900

    mypage layout

---
+++ src/main/webapp/html/mypage/index.js
@@ -0,0 +1,12 @@
+var param = location.href.split('?')[1],
+pageNo = 1,
+pageSize = 6
+
+
+// JSON 형식의 데이터 가져오기
+
+// message
+$('.sb-message').click((e) => {
+  window.location.href = '../message/index.html'
+});
+
