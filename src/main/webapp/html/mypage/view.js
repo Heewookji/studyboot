f2a6f89cd174c166d59756a2de69a3fce97122f6
ppappikko <sanghyun.dev@gmail.com>
2019-05-26 18:18:47 +0900

    mypage 메인 수정중, 내정보 페이지 수정중..

--- src/main/webapp/html/mypage/view.js
+++ src/main/webapp/html/mypage/view.js
@@ -1,12 +1,16 @@
- var param = location.href.split('?')[1];
+var param = location.href.split('?')[1];
 
 
 // JSON 형식의 데이터 가져오기
 
 
+// message
+$('.sb-message').click((e) => {
+  window.location.href = '../message/index.html'
+});
+
 // inquiry - 작업중
 $('#inqryForm-btn').click((e) => {
-  e.preventDefault();
   $('.sspctForm-Format').addClass('std-invisible');
   $('#inqryName').html("홍길동");
   $('#formTitle').html($('#inqryName').html() +"님  문의"+ " 내용을 적어주세요");
