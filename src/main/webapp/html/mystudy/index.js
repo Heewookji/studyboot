1caf521e7457e53ff2896ddfd6f9a3c17a488314
82105 <sangmin.P42@gmail.com>
2019-05-27 15:51:22 +0900

    mystudy photo
    스터디 사진 불러오기 완료

--- src/main/webapp/html/mystudy/index.js
+++ src/main/webapp/html/mystudy/index.js
@@ -1,69 +1,58 @@
-
-$( document ).ready(function() {
-    $("#js-header").load("../header.html", function(){
-	$(document.body).trigger('loaded-header');
-    });
-});
-
-
-$(document.body).bind('loaded-header', () => {
-    // initialization of go to
-    $.HSCore.components.HSGoTo.init('.js-go-to');
-  
-    // initialization of HSDropdown component
-    $.HSCore.components.HSDropdown.init($('[data-dropdown-target]'), {
-	afterOpen: function(){
-	    $(this).find('input[type="search"]').focus();
-	}
-    });
-
-    // initialization of masonry
-    $('.masonry-grid').imagesLoaded().then(function () {
-	$('.masonry-grid').masonry({
-	    columnWidth: '.masonry-grid-sizer',
-	    itemSelector: '.masonry-grid-item',
-	    percentPosition: true
-	});
-    });
-
-    // initialization of popups
-    $.HSCore.components.HSPopup.init('.js-fancybox');
-
-    // initialization of header
-    $.HSCore.components.HSHeader.init($('#js-header'));
-    $.HSCore.helpers.HSHamburgers.init('.hamburger');
-    
-
- // initialization of carousel
-    $.HSCore.components.HSCarousel.init('[class*="js-carousel"]');
-
-
-});
-
-
-function getCurrentScrollPercentage(){
-    return (window.scrollY + window.innerHeight) / document.body.clientHeight * 100
-}
+var param = location.href.split('?')[1],
+//no = param.split('=')[1];
+// 위에 코드가 맞는 거!! 임시로 no 값 1 넣어놨습니다.
+// 원래 클릭하면 스터디 번호 받아와서 값에 넣어줘야함!
+no = 1;
 
 //JSON 형식의 데이터 목록 가져오기
 function loadList(no) {
-  $.getJSON('../../app/json/study/getStudy?no=' + no,
+  $.getJSON('../../app/json/study/studyphoto?no=' + no,
       function(obj) {
 
     console.log(no);
+    $('#study_img').attr("src", "/studyboot/upload/images/" + obj.photo);
 
-
-
-    $(cardGenerator(obj)).appendTo(tbody);
-
+    
     // 데이터 로딩이 완료되면 body 태그에 이벤트를 전송한다.
     $(document.body).trigger('loaded-list');
       });
 
 };
 
+loadList(no);
 
 
+$(document).on('ready', function () {
+
+  // initialization of tabs
+  $.HSCore.components.HSTabs.init('[role="tablist"]');
+
+  // initialization of rating
+  $.HSCore.components.HSRating.init($('.js-rating'), {
+    spacing: 2
+  });
+
+  // initialization of HSMegaMenu component
+  $('.js-mega-menu').HSMegaMenu({
+    event: 'hover',
+    pageContainer: $('.container'),
+    breakpoint: 991
+  });
+
+  // initialization of horizontal progress bars
+  setTimeout(function() { // important in this case
+    var horizontalProgressBars = $.HSCore.components.HSProgressBar.init('.js-hr-progress-bar', {
+      direction: 'horizontal',
+      indicatorSelector: '.js-hr-progress-bar-indicator'
+    });
+  }, 1);
+});
+
+$(window).on('resize', function () {
+  setTimeout(function () {
+    $.HSCore.components.HSTabs.init('[role="tablist"]');
+  }, 200);
+});
 
 
 
