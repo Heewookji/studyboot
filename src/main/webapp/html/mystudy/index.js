6579833001c42a7cd7557a9d94c3df280197968f
82105 <sangmin.P42@gmail.com>
2019-05-27 09:14:07 +0900

    mystudy index

---
+++ src/main/webapp/html/mystudy/index.js
@@ -0,0 +1,62 @@
+
+$( document ).ready(function() {
+    $("#js-header").load("../header.html", function(){
+	$(document.body).trigger('loaded-header');
+    });
+});
+
+
+$(document.body).bind('loaded-header', () => {
+    // initialization of go to
+    $.HSCore.components.HSGoTo.init('.js-go-to');
+  
+    // initialization of HSDropdown component
+    $.HSCore.components.HSDropdown.init($('[data-dropdown-target]'), {
+	afterOpen: function(){
+	    $(this).find('input[type="search"]').focus();
+	}
+    });
+
+    // initialization of masonry
+    $('.masonry-grid').imagesLoaded().then(function () {
+	$('.masonry-grid').masonry({
+	    columnWidth: '.masonry-grid-sizer',
+	    itemSelector: '.masonry-grid-item',
+	    percentPosition: true
+	});
+    });
+
+    // initialization of popups
+    $.HSCore.components.HSPopup.init('.js-fancybox');
+
+    // initialization of header
+    $.HSCore.components.HSHeader.init($('#js-header'));
+    $.HSCore.helpers.HSHamburgers.init('.hamburger');
+    
+
+ // initialization of carousel
+    $.HSCore.components.HSCarousel.init('[class*="js-carousel"]');
+
+
+});
+
+
+
+$(document.body).bind('loaded-header', () => {
+    $(window).scroll(function(obj) {
+	const currentScrollPercentage = getCurrentScrollPercentage()
+
+	if(currentScrollPercentage > 50){
+	    $('#header-child').prop('hidden', false);
+	    $('#header-search').prop('hidden', false);
+	}else{
+	    $('#header-child').prop('hidden', true);
+	    $('#header-search').prop('hidden', true);
+	}
+    });
+});
+
+
+function getCurrentScrollPercentage(){
+    return (window.scrollY + window.innerHeight) / document.body.clientHeight * 100
+}
