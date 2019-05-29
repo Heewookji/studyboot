637642ce549fef0147dd9176c72d32fa982da5bc
jihwan8D <jihwan8D@gmail.com>
2019-05-29 18:23:09 +0900

    마이스터디 캘린더 form 리셋

--- src/main/webapp/html/mystudy/calendar/calendar.js
+++ src/main/webapp/html/mystudy/calendar/calendar.js
@@ -32,29 +32,46 @@ document.addEventListener('DOMContentLoaded', function() {
 
 /* ------------------- */
 
-//날짜를 눌렀을때 일정 버튼이 눌려서, 일정을 추가할 수 있고, 선택한 날짜 자동으로 입력됨.
+//날짜를 눌렀을때 일정추가 모달 버튼이 눌려서, 일정을 추가할 수 있고, 선택한 날짜 자동으로 입력됨.
 $(document.body).bind('dateClick', () => {
-  $('#schedule-name').attr("value", "");
+  //$('#schedule-title').empty();
+  //$('#schedule-title').value=null;
+  //$('#schedule-title').val("");
+  $('#schedule-title').attr("value", "");
   $('#schedule-edt').attr("value", "");
-  $('#schedule-memo').html("");
+  $('#schedule-memo').empty();
 
   $('#schedule-add-btn').show(); // 모달의 수정 버튼 보이게
   $('#schedule-update-btn').hide(); // 모달의 수정 버튼 숨김
-  
+
   //$('schedule-update-btn').attr("hidden", hidden);
   $('#calendar-add-modal-btn').click();
   $('#schedule-sdt').attr("value", window.dateStr + 'T00:00'); // 클릭한 날짜 + 00시 00분 으로 초기 세팅
 });
+/* --------------------------------------------------------------------------------------------- */
 
-
-// 일정을 등록하는 버튼
+//일정을 등록하는 버튼
 $('#schedule-add-btn').click(() => {
+
+  if($('#schedule-title').val().length === 0) {
+    alert("일정 제목을 입력해 주세요.");
+    return;
+  } else if($('#schedule-sdt').val().length === 0) {
+    alert("일정 시작일을 입력해 주세요.");
+    return;
+  } else if($('#schedule-edt').val().length === 0) {
+    alert("일정 종료일을 입력해 주세요.");
+    return;
+  } else if($('#schedule-memo').val().length === 0) {
+    alert("일정 내용을 입력해 주세요.");
+    return;
+  }
 
   $.ajax({
     url : "../../app/json/mystudyschedule/add",
     type : "post",
     data : {
-      title: $('#schedule-name').val(), // 좌항은 프러퍼티명 , 우항은 프러퍼티에 담을 값
+      title: $('#schedule-title').val(), // 좌항은 프러퍼티명 , 우항은 프러퍼티에 담을 값
       studyNo: 1,
       memberNo: 2,
       start: $('#schedule-sdt').val(),
@@ -73,10 +90,8 @@ $('#schedule-add-btn').click(() => {
   });
 });
 
-// 달력의 일정을 클릭하면 모달 버튼을 눌리게 함
+//달력의 일정을 클릭하면 모달 버튼을 눌리게 함
 $(document.body).bind('eventClick', () => {
-  //console.log(window.eventDate.id);
-  //console.log(window.eventDate.title);
   $('#calendar-detail-modal-btn').click();
 });
 
@@ -86,7 +101,7 @@ function loadDetail(no) {
     console.log(obj); // 콘솔에 해당 키값의 디테일 값 잘 출력됨
 
     updateStartDate = obj.start.substring(0, 10); // 업데이트때 사용할 년, 월, 일
-    updateEndDate = obj.start.substring(0, 10); // 업데이트때 사용할 년, 월, 일
+    updateEndDate = obj.end.substring(0, 10); // 업데이트때 사용할 년, 월, 일
 
     var startM = obj.start.substring(5, 7); // 월
     var startD = obj.start.substring(8, 10); // 일
@@ -119,22 +134,25 @@ $('#event-delete-btn').click(() => {
   }
 });
 
-// 디테일에서 수정누르면 실행되는 곳. 
+/* ------------------------------------------------------------------------------------------------ */
+// 디테일에서 수정버튼 누르면 실행되는 곳. 
 $('#event-update-btn').click(() => {
-  $('#schedule-memo').html(""); // memo 누적안되게 맨처음 초기화
-
+  
+  $('#schedule-memo').empty(); // textarea(memo)에 누적이 안되게 일단 비워준다.
+  //$('#schedule-title').attr("value", "");
+//  $('#schedule-title').val("asf");
+  resetForm();
   $('#schedule-update-btn').show(); // 모달의 수정 버튼 보이게
   $('#schedule-add-btn').hide(); // 모달의 등록 버튼 숨김
 
   $('#calendar-detail-close-modal-btn').click(); // 디테일 모달 닫고,
   $('#calendar-add-modal-btn').click(); // 수정하는 모달 띄움
 
-  $('#schedule-name').attr("value", updateTitleDate); // 디테일에 있던 이름 모달창으로 가져옴
+  //$('#schedule-title').val(updateTitleDate); // 디테일에 있던 일정 이름 모달창으로 가져옴
+  $('#schedule-title').attr("value", updateTitleDate); // 디테일에 있던 일정 이름 모달창으로 가져옴
   $('#schedule-sdt').attr("value", updateStartDate + 'T00:00');
   $('#schedule-edt').attr("value", updateEndDate + 'T00:00');
-
-  $('#schedule-memo').attr("value", '내용'); // 클릭한 날짜 + 00시 00분 으로 초기 세팅
-  $('#schedule-memo').append(updateMemoDate);
+  $('#schedule-memo').append(updateMemoDate); // 디테일에 있던 일정 내용 모달창으로 가져옴
 });
 
 
@@ -144,7 +162,7 @@ $('#schedule-update-btn').click(() => {
     type : "post",
     data : {
       id: window.eventDate.id,
-      title: $('#schedule-name').val(), // 좌항은 프러퍼티명 , 우항은 프러퍼티에 담을 값
+      title: $('#schedule-title').val(), // 좌항은 프러퍼티명 , 우항은 프러퍼티에 담을 값
       studyNo: 1,
       memberNo: 2,
       start: $('#schedule-sdt').val(),
@@ -162,3 +180,13 @@ $('#schedule-update-btn').click(() => {
     }
   });
 });
+
+
+$('#schedule-close-btn').click(() => {
+  $('#schedul-submit-form')[0].reset();
+});
+
+function resetForm() {
+    $('#schedul-submit-form')[0].reset();
+  };
+
