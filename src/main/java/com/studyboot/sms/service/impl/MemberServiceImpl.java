3939063889eea4b3afadbb81b88cd2594b3b3286
heewookji <wlgmldnr@daum.net>
2019-06-05 11:08:11 +0900

    error

--- src/main/java/com/studyboot/sms/service/impl/MemberServiceImpl.java
+++ src/main/java/com/studyboot/sms/service/impl/MemberServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import com.studyboot.sms.dao.AppliedStudyDao;
 import com.studyboot.sms.dao.MemberDao;
 import com.studyboot.sms.dao.StudyDao;
