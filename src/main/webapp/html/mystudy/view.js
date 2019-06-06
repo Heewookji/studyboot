fa14252c5e0af98e7e57445fb888ca8e7f67ee2f
82105 <sangmin.P42@gmail.com>
2019-06-07 08:26:00 +0900

    mystudy board detail

--- src/main/webapp/html/mystudy/view.js
+++ src/main/webapp/html/mystudy/view.js
@@ -1,25 +1,41 @@
 
 
 
+function loadDetail(no) {
+  $.getJSON('../../app/json/MyStudy/detail?no=' + no, function(obj) {
+
+    console.log(obj);
+    
+    $('#detail-photo').attr("src", "/studyboot/upload/images/" + obj.member.photo);
+    $('#detail-nick').html(obj.member.nickName);
+    $('#detail-date').html(obj.date);
+    $('#detail-title').html(obj.title);
+    $('#detail-cont').html(obj.contents);
+    
+    $(document.body).trigger('loaded-detail');
+
+  });
+
+}
 
 
 //board 핸들바스 사용해 리스트 전부 출력완료
 //$(document.body).bind('loaded-list', () => {
-//  alert("view.js");
-//  $('.detail-btn').click((e) => {
-//    e.preventDefault();
-//    loadDetail($(e.target).attr('data-no'));
-//  });
+//alert("view.js");
+//$('.detail-btn').click((e) => {
+//e.preventDefault();
+//loadDetail($(e.target).attr('data-no'));
+//});
 //});
 
-// loadDetail 전부 완료된 후 
+//loadDetail 전부 완료된 후 
 //$(document.body).bind('loaded-detail', () => {
-//  
-//  $("#contents").load("/studyboot/html/mystudy/view.html", function(){
-//    
-//  });
+
+//$("#contents").load("/studyboot/html/mystudy/view.html", function(){
+
 //});
-//
+//});
+
 
 /*
  $('.loaded-detail').click((e) => {
@@ -38,13 +54,13 @@
 
 
 $(document.body).bind('loaded-list', () => {
-$('.detail-btn').click((e) => {
-  $("#contents").load("/studyboot/html/mystudy/view.html", function(){
-    
-    loadDetail($(e.target).attr('data-no'));
-    
+  $('.detail-btn').click((e) => {
+    $("#contents").load("/studyboot/html/mystudy/view.html", function(){
+
+      loadDetail($(e.target).attr('data-no'));
+
+    });
   });
-});
 });
 
 
