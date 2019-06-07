6b69413f362c4d93fd0a76776bdbb0dcc6e191a7
heewookji <wlgmldnr@daum.net>
2019-06-07 10:50:25 +0900

    heewook - login ex password

--- src/main/java/com/studyboot/sms/web/json/MailController.java
+++ src/main/java/com/studyboot/sms/web/json/MailController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.studyboot.sms.domain.Member;
+import com.studyboot.sms.service.MemberService;
 
 @RestController("json/MailController")
 @RequestMapping("/json/mail")
@@ -19,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
   
       @Autowired 
       private JavaMailSender mailSender;
+      @Autowired 
+      MemberService memberService;
       private String from = "wlgmldnr@naver.com";
       private String subject = "studyboot invites you!";
       
@@ -56,4 +60,52 @@ import org.springframework.web.bind.annotation.RestController;
           return content;
       }
       
+      
+      
+      @GetMapping("sendpasswordmail")
+      public Object sendPasswordMail(@RequestParam String email) {
+          
+        HashMap<String,Object> content = new HashMap<>();
+        
+        Member member = memberService.get(email);
+        
+        if(member != null) {
+           
+          try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+            messageHelper.setTo(email);
+            
+            String id = String.valueOf((int)(Math.random() * 10000000));
+            
+            messageHelper.setText("임시비밀번호는 " + id + " 입니다! \n"
+                + "로그인 후 꼭 비밀번호를 다시 변경해주세요!");
+            
+            
+            messageHelper.setFrom(from);
+            messageHelper.setSubject("studyboot: 임시 비밀번호 발송");  // 메일제목은 생략이 가능하다
+            
+            
+            mailSender.send(message);
+            
+            content.put("status", "success");
+            content.put("id", id);
+            
+            member.setPassword(id);
+            
+            memberService.update(member);
+            
+        } catch(Exception e){
+          
+          content.put("status", "fail");
+        }
+          
+        } else {
+          content.put("status", "fail");
+        }
+        
+        
+          
+          return content;
+      }
   }
