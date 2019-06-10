90fe7cc25b03bbede2c7c894198c4fe2be3a4d50
jihwan8D <jihwan8D@gmail.com>
2019-06-10 14:27:33 +0900

    탈퇴자 평가 ing..

--- src/main/java/com/studyboot/sms/dao/RateDao.java
+++ src/main/java/com/studyboot/sms/dao/RateDao.java
@@ -2,11 +2,14 @@ package com.studyboot.sms.dao;
 
 import java.util.List;
 import java.util.Map;
+import com.studyboot.sms.domain.Member;
 import com.studyboot.sms.domain.Rate;
 import com.studyboot.sms.domain.RateLog;
 
 public interface RateDao {
   List<Rate> findAll(Map<String, Object> params);
   int evaluationAdd(Map<String, Object> evaluationMap);
+  int rateRequireUpdate(Map<String, Object> rateRequireMap);
   List<RateLog> findRateLog(int no);
+  int insertRateLog(Member member);
 }
