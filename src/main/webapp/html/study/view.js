56505d26eaf9c030980b1040ae045e77033467a0
heewookji <wlgmldnr@daum.net>
2019-06-18 18:17:22 +0900

    heewook - chart

--- src/main/webapp/html/study/view.js
+++ src/main/webapp/html/study/view.js
@@ -20,6 +20,11 @@ if (param) {
 
   //이름 타이틀에 넣어준다.
   $('#studyNameHead').html(decodeURIComponent(name.split('=')[1]));
+
+
+  //차트를 준비합니다람쥐.
+  loadChart(studyNo);
+
 }
 
 
@@ -45,7 +50,7 @@ function loadData(no) {
 
 
     $('#name').text(data.name);
-    $('#goal').html( data.goal);
+    $('#goal').html(data.goal);
     $('#contents').append(data.contents);
 
     for(var day of data.dayStrList){
@@ -94,13 +99,13 @@ function loadData(no) {
 
 
     //지도 출력
-    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    var options = { //지도를 생성할 때 필요한 기본 옵션
-            center: new daum.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-            level: 3 //지도의 레벨(확대, 축소 정도)
-    };
-
-    var map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴
+//    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+//    var options = { //지도를 생성할 때 필요한 기본 옵션
+//            center: new daum.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+//            level: 3 //지도의 레벨(확대, 축소 정도)
+//    };
+//
+//    var map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴
 
     $.getJSON('../../app/json/member/mystudy',
             function(mystudyData) {
@@ -154,6 +159,207 @@ function loadData(no) {
     });
 
   });
+}
+
+
+
+
+//스터디 차트데이터를 불러오는 함수
+function loadChart(no) {
+
+  var c = document.getElementById("studyRateChart");
+  var studyRateChart = c.getContext("2d");
+  var gradientStroke = studyRateChart.createLinearGradient(500, 0, 100, 0);
+  gradientStroke.addColorStop(0, 'rgba(128, 182, 244, 0.7)');
+  gradientStroke.addColorStop(1, 'rgba(244, 144, 128, 0.7)');
+  var gradientFill = studyRateChart.createLinearGradient(500, 0, 100, 0);
+  gradientFill.addColorStop(0, "rgba(128, 182, 244, 0.3)");
+  gradientFill.addColorStop(1, "rgba(244, 144, 128, 0.3)");
+
+
+  var studyRateChartShow = new Chart(studyRateChart, {
+    type: 'line',
+    data: {
+      labels: ['0', '1', '2', '3', '4', '5'],
+      datasets: [{
+        borderColor: gradientStroke,
+        pointBorderColor: gradientStroke,
+        pointBackgroundColor: gradientStroke,
+        pointHoverBackgroundColor: gradientStroke,
+        pointHoverBorderColor: gradientStroke,
+        pointHoverRadius: 10,
+        pointHoverBorderWidth: 1,
+        pointRadius: 5,
+        fill: true,
+        backgroundColor: gradientFill,
+        borderWidth: 4,
+        data: [
+          10, 20, 30, 40 , 20, 0
+          ],
+      }]
+    },
+    options: {
+      legend: false,
+      elements: {
+        point: {
+          pointStyle: 'circle'
+        }
+      },
+      responsive: true,
+      title: {
+        display: false,
+        text: 'Chart.js Line Chart'
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      scales: {
+        xAxes: [{
+          gridLines: {
+            display: true,
+            drawBorder: true,
+            drawOnChartArea: false,
+          },
+          scaleLabel: {
+            display: false,
+            labelString: 'Month'
+          }
+        }],
+        yAxes: [{
+          gridLines: {
+            display: false,
+            drawBorder: false,
+            drawOnChartArea: false,
+          },
+          scaleLabel: {
+            display: false,
+            labelString: 'Value'
+          },
+          ticks: {
+            display: false
+          }
+        }]
+      }
+    }
+  });
+
+  var ageRateChart = $("#ageRateChart");
+
+  var myBarChart = new Chart(ageRateChart, {
+    type: 'horizontalBar',
+    data: {
+      labels: ['10', '20', '30', '40', '50', '60'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 30, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(153, 102, 255, 0.5)',
+            'rgba(255, 159, 64, 0.5)'
+            ],
+            borderWidth: 4
+      }]
+    },
+    options: {
+      legend: false,
+      elements: {
+        point: {
+          pointStyle: 'circle'
+        }
+      },
+      responsive: true,
+      title: {
+        display: false,
+        text: 'Chart.js Line Chart'
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      scales: {
+        yAxes: [{
+          gridLines: {
+            display: true,
+            drawBorder: true,
+            drawOnChartArea: false,
+          },
+          scaleLabel: {
+            display: false,
+            labelString: 'Month'
+          }
+        }],
+        xAxes: [{
+          gridLines: {
+            display: false,
+            drawBorder: false,
+            drawOnChartArea: false,
+          },
+          scaleLabel: {
+            display: false,
+            labelString: 'Value'
+          },
+          ticks: {
+            display: false
+          }
+        }]
+      }
+    }
+  });
+
+  var ageRateChart = $("#attendRateChart");
+
+//For a pie chart
+  var myPieChart = new Chart(ageRateChart, {
+    type: 'pie',
+    data: {
+      
+      datasets: [{
+        data: [10, 20, 30],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)'
+            ],
+          borderWidth: 4
+      }],
+
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: [
+        '탈퇴',
+        '완료',
+        '추방'
+        ]
+    },
+    options: {
+    }
+
+  });
+
 }
 
 
@@ -223,9 +429,9 @@ $(document.body).bind('loaded-studyInfo', () => {
 
 $('#applyAdd-btn').click(function(e){
 
-  if($( "#goal" ).val().length <= 30){
+  if($( "#userGoal" ).val().length <= 30){
 
-    $.getJSON('../../app/json/study/appliedStudy?studyNo=' + studyNo + '&determination='+ $( "#goal" ).val(),
+    $.getJSON('../../app/json/study/appliedStudy?studyNo=' + studyNo + '&determination='+ $( "#userGoal" ).val(),
             function(data) {
       if(data.status == "success"){
         Swal.fire({
@@ -259,41 +465,23 @@ $('#applyAdd-btn').click(function(e){
 });
 
 //목표체크
-$( "#goal" ).keyup(function(){
-  if($( "#goal" ).val().length > 30){
-    $("#goal").attr("data-toggle","tooltip");
-    $("#goal").attr("data-trigger","hover focus");
-    $("#goal").attr("data-placement","bottom");
-    $("#goal").attr("data-html", true);
-    $("#goal").attr("title","30자 이내로<br>목표를 입력해주세요!");
-    $('#goal').tooltip('enable');
-    $('#goal').tooltip('show');
+$( "#userGoal" ).keyup(function(){
+  if($( "#userGoal" ).val().length > 30){
+    $("#userGoal").attr("data-toggle","tooltip");
+    $("#userGoal").attr("data-trigger","hover focus");
+    $("#userGoal").attr("data-placement","bottom");
+    $("#userGoal").attr("data-html", true);
+    $("#userGoal").attr("title","30자 이내로<br>목표를 입력해주세요!");
+    $('#userGoal').tooltip('enable');
+    $('#userGoal').tooltip('show');
   } else{
-    if($("#goal").attr("data-toggle")){
-      $('#goal').tooltip('disable');
-      $('#goal').tooltip('hide');
+    if($("#userGoal").attr("data-toggle")){
+      $('#userGoal').tooltip('disable');
+      $('#userGoal').tooltip('hide');
     }
   }
 });
 
-
-
-//drop down 샘플 ------------------
-
-
-$('.ui.fluid.normal.dropdown.sample')
-.dropdown({
-  maxSelections: 3
-})
-;
-
-$('.ui.dropdown.sample')
-.dropdown({
-  on: 'hover',
-  onChange: function(value, text, $selectedItem) {
-    $('#sample1').append('<a class="ui label transition visible" data-no="'+value+'" style="display: inline-block !important;">'+text+'<i class="delete icon"></i></a>');
-  }
-});
 
 
 
